489e01da0f03e1d2e9b0fe547a832a4f7dd240d3
Jmbualungu <155210863+Jmbualungu@users.noreply.github.com>
2026-01-18 21:52:09 -0500

    Initial Commit

---
+++ Investor Tool/Investor_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Investor_ToolApp.swift
+//  Investor Tool
+//
+//  Created by James Mbualungu on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Investor_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
